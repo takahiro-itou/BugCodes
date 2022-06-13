d4e7d1b9d28e61f819702cd540bff3c1e252ec82
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2022-06-13 23:58:13 +0900

    関数の return を書かず gcc 9.4.0 で最適化 O2 を有効にする
    (1) 無限ループ (無限再帰呼び出し) によりコアダンプ

---
+++ optimize/noreturn.cpp
@@ -0,0 +1,23 @@
+
+#if 0
+g++ -O2 -g noreturn.cpp
+exit 0;
+#endif
+
+#include    <iostream>
+
+int count = 0;
+
+int bug(int x) {
+    std::cerr   <<  "count = "  <<  ++ count << std::endl;
+}
+
+int infinite_loop_when_O2(int x) {
+    int ret;
+    ret = bug(x);
+    return ret;
+}
+
+int main(int argc, char * argv[]) {
+    infinite_loop_when_O2(1);
+}
